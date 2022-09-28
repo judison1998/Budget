ba3a6dd18ff796ed0e5bbd937803dcc1b9e3bb91
kellyketahr17 <kellykethar17@gmail.com>
2022-09-28 15:29:39 +0300

    ListActivity has been modified

--- app/src/main/java/com/example/budget/ListActivity.kt
+++ app/src/main/java/com/example/budget/ListActivity.kt
@@ -2,13 +2,34 @@ package com.example.budget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListActivity : AppCompatActivity() {
 
+    lateinit var listRV : RecyclerView
+    lateinit var budgetAdapter: BudgetAdapter
+    lateinit var listItem : ArrayList<ItemModal>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        listRV = findViewById(R.id.list_recyclerview)
+
+        listItem = ArrayList()
+        val layoutManager = GridLayoutManager(this, 2)
+        listRV.layoutManager = layoutManager
+        budgetAdapter = BudgetAdapter(listItem,this)
+        listRV.adapter = budgetAdapter
+
+        listItem.add(ItemModal("Chairs",200000,R.drawable.chairs))
+        listItem.add(ItemModal("Cups",10000,R.drawable.cups))
+        listItem.add(ItemModal("Kettle",75000,R.drawable.packos))
+        listItem.add(ItemModal("Plates",5000,R.drawable.plates))
+        listItem.add(ItemModal("Spoons",1000,R.drawable.spoons))
+
+        budgetAdapter.notifyDataSetChanged()
 
     }
 }
