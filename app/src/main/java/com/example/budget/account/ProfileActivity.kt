233795d7274b43deec5d6754bad07327c98357bd
judison256 <codewithjudison256@gmail.com>
2022-10-13 17:09:01 +0300

    google sign in, app icon

--- app/src/main/java/com/example/budget/account/ProfileActivity.kt
+++ app/src/main/java/com/example/budget/account/ProfileActivity.kt
@@ -3,11 +3,22 @@ package com.example.budget.account
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
+import com.example.budget.ListActivity
+import com.example.budget.R
 
 import com.example.budget.databinding.ActivityProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
     private lateinit var binding: ActivityProfileBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
@@ -21,27 +32,48 @@ class ProfileActivity : AppCompatActivity() {
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
         firebaseAuth = FirebaseAuth.getInstance()
+
         binding.signOut.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
+
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(this, LoginActivity::class.java)
+                Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }
         }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
     }
 
     override fun onStart() {
         super.onStart()
-
         if (firebaseAuth.currentUser != null) {
-            val user = firebaseAuth.currentUser
-            val name = user?.displayName
-            val emailAdress = user?.email
+            var user = firebaseAuth.currentUser
+            var name = user?.displayName
+            var emailAdress = user?.email
             binding.emailAddress.text = emailAdress.toString()
             binding.name.text = name
 
-        } else {
+        }
+        else if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            binding.emailAddress.text = GoogleSignIn.getLastSignedInAccount(this)?.email
+            binding.name.text = GoogleSignIn.getLastSignedInAccount(this)?.displayName
+          //val userEmail = binding.emailAddress.setText(account.email.toString())
+
+        }
+        else {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
     }
